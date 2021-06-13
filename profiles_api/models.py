0c30fc5dee96afdda0cce63fc05829497fda4760
ashik155 <ashikpatel444@gmail.com>
2021-06-12 21:52:26 -0400

    DB_AND_CUSTOM_USER_SETUP

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+#these two  are important to overrride django by defualt user or usrr AuthenticationMiddleware system
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManager(BaseUserManager):
+    '''Creating A normal User '''
+
+    def create_user(self,email,name,password=None):
+        '''creating a normal user'''
+        if not email:
+            raise ValueError("User Must have an email address....")
+        
+        email = self.normalize_email(email)
+        user =  self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self,email,name,password):
+        '''creating a new superuser'''
+        user = self.create_user(email,name,password)
+        user.is_superuser=True
+        user.is_staff= True
+        user.save(using=self._db)
+
+        return user
+
+
+
+
+
+
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """ Databse model for users in the System """
+
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    #idontknow why ?
+    objects = UserProfileManager()
+
+    #mentioning or here we are overrriding django user Authentication
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    #mentining few ability to UserProfile
+
+    def getFullName(self):
+        '''Retrieve Full name '''
+        return self.name
+
+    def getShortName(self):
+        '''retrive short name of user '''
+        return self.name 
+
+    def __str__(self):
+        '''returnign String Representation of our user'''
+        return self.email
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #these 3 are useful as we are using this for our api buildings....
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_api',
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
